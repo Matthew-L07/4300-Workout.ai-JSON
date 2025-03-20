4258ff6f404033451a087725135f18d0bebf37c8
Jake Ludgin <jml557@cornell.edu>
2025-03-19 22:48:59 -0400

    started tf-idf logic

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 import json
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 json_file_path = os.path.join(current_directory, 'init.json')
@@ -13,12 +15,31 @@ with open(json_file_path, 'r') as file:
     data = json.load(file)
     exercises_df = pd.DataFrame(data['Exercises'])
 
-equipment_list = sorted(exercises_df['Equipment'].dropna().unique().tolist())
-bodypart_list = sorted(exercises_df['BodyPart'].dropna().unique().tolist())
-level_list = sorted(exercises_df['Level'].dropna().unique().tolist())
+    documents = [
+        (
+            pt["Title"].upper(),
+            pt["Desc"],
+            pt["BodyPart"],
+            pt["Equipment"],
+            pt["Level"],
+            pt["Rating"],
+            pt["RatingDesc"]
+        )
+        for pt in exercises_df
+    ]
+
+    equipment_list = sorted(
+        exercises_df['Equipment'].dropna().unique().tolist())
+    bodypart_list = sorted(exercises_df['BodyPart'].dropna().unique().tolist())
+    level_list = sorted(exercises_df['Level'].dropna().unique().tolist())
+
+    vectorizer = TfidfVectorizer(stop_words='english')
+    term_document_matrix = vectorizer.fit_transform(x[1] for x in documents)
+
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/")
 def home():
@@ -27,30 +48,48 @@ def home():
                            bodypart_list=bodypart_list,
                            level_list=level_list)
 
+
 @app.route("/exercises")
 def exercises_search():
-    text = request.args.get("title", "")
-    equipment = request.args.get("equipment", "")
-    bodypart = request.args.get("bodypart", "")
-    level = request.args.get("level", "")
+    # text = request.args.get("title", "")
+    # equipment = request.args.get("equipment", "")
+    # bodypart = request.args.get("bodypart", "")
+    # level = request.args.get("level", "")
+    query = request.args.get("query", "").strip().lower()
+
+    if not query:
+        return None
+
+    queryVec = vectorizer.transform([query])
+
+    score = cosine_similarity(queryVec, term_document_matrix).flatten()
+
+    topResults = score.argsort()[::-1][:10]
 
     filtered_df = exercises_df
 
-    if text:
-        filtered_df = filtered_df[filtered_df['Title'].str.lower().str.contains(text.lower())]
+    results = filtered_df.iloc[topResults][[
+        'Title', 'Desc', 'BodyPart', 'Equipment', 'Level', 'Rating']].to_dict(orient='records')
 
-    if equipment:
-        filtered_df = filtered_df[filtered_df['Equipment'] == equipment]
+    return jsonify(results)
 
-    if bodypart:
-        filtered_df = filtered_df[filtered_df['BodyPart'] == bodypart]
+    # if text:
+    #     filtered_df = filtered_df[filtered_df['Title'].str.lower(
+    #     ).str.contains(text.lower())]
 
-    if level:
-        filtered_df = filtered_df[filtered_df['Level'] == level]
+    # if equipment:
+    #     filtered_df = filtered_df[filtered_df['Equipment'] == equipment]
 
-    matches_filtered = filtered_df[['Title', 'Desc', 'Rating']]
-    matches_filtered_json = matches_filtered.to_json(orient='records')
-    return matches_filtered_json
+    # if bodypart:
+    #     filtered_df = filtered_df[filtered_df['BodyPart'] == bodypart]
+
+    # if level:
+    #     filtered_df = filtered_df[filtered_df['Level'] == level]
+
+    # matches_filtered = filtered_df[['Title', 'Desc', 'Rating']]
+    # matches_filtered_json = matches_filtered.to_json(orient='records')
+    # return matches_filtered_json
+
 
 if 'DB_NAME' not in os.environ:
     app.run(debug=True, host="0.0.0.0", port=5000)
